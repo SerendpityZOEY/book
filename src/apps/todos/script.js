d90f924cf5440fb2df778e4a3baefe97055e95fe
SerendpityZOEY <zoey.zhang513@hotmail.com>
2016-01-18 18:34:17 -0700

    Hackathon

---
+++ src/apps/todos/script.js
@@ -0,0 +1,10 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " The APP is made by a greate team of CU Boulder. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='Low.html'>Low</a></li>" +
+"<li><a href='Medium.html'>Medium</a></li>" +
+"<li><a href='High.html'>High</a></li>" +
+"</ul>";
