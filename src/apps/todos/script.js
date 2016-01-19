0609cc16aa08bca030f8c2900df7cfd68164df6f
SerendpityZOEY <zoey.zhang513@hotmail.com>
2016-01-19 01:19:00 -0700

    UpdateHackathon

--- src/apps/todos/script.js
+++ src/apps/todos/script.js
@@ -2,9 +2,16 @@ document.getElementById("foot01").innerHTML =
 "<p>&copy;  " + new Date().getFullYear() + " The APP is made by a greate team of CU Boulder. All rights reserved.</p>";
 
 document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" +
+"<div class='nav-wrapper'>"+
+"<a href='#' class='brand-logo right'>UCDD2016</a> "+
+"<ul id='menu' class='left hide-on-med-and-down' >" +
 "<li><a href='index.html'>Home</a></li>" +
 "<li><a href='Low.html'>Low</a></li>" +
 "<li><a href='Medium.html'>Medium</a></li>" +
 "<li><a href='High.html'>High</a></li>" +
-"</ul>";
+"<li><form method='get' action='http://google.com/search'><div class='input-field'><input id='search' type='search' name='q' alt='Search' required><label for='search'><i class='material-icons'>search</i></label><i class='material-icons'>close</i></div></form></li>"+
+"</ul>"+
+"</div>";
+
+
+
