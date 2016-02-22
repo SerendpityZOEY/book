0a4baa519b0865f4447c06e2a80c06c2e622de49
SerendpityZOEY <zoey.zhang513@hotmail.com>
2016-02-21 23:18:46 -0700

    updateUber

--- src/apps/uber/client/components/map.jsx
+++ src/apps/uber/client/components/map.jsx
@@ -3,22 +3,52 @@ const {Map, Marker, CircleMarker, Popup, TileLayer, MapLayer}  = window.ReactLea
 class MapView extends React.Component {
   render(){
 
-    const providers = this.props.providers
+    const providers = this.props.providers;
+    const restaurants = this.props.restaurants;
+
     const providerElements = _.map(providers, function(p,i){
-      
+
       var pos = [p.lat, p.lon];
-      return <Marker position={pos} key={i}>
+
+      var provider_icon = L.icon({
+      iconUrl: 'images/providerIcon.png',
+      iconSize: [40, 40],
+      iconAnchor: [0, 40],
+      popupAnchor: [-3, -76]
+      })
+
+      return <Marker position={pos} key={i} icon={provider_icon}>
         <Popup>
-          <span><b> {p.name}</b></span>
+          <span><font color="black"><b> {p.name}</b></font></span>
         </Popup>
       </Marker>
     })
 
-    let userElement
+
+   const restaurantsElements = _.map(restaurants, function(r,i){
+
+      var pos = [r.lat, r.lon];
+
+      var restaurant_icon = L.icon({
+      iconUrl: 'images/restaurantIcon.png',
+      iconSize: [40, 40],
+      iconAnchor: [0, 40],
+      popupAnchor: [-3, -76]
+      })
+
+      return <Marker position={pos} key={i} icon={restaurant_icon}>
+        <Popup>
+          <span><font color="black"> <b> {r.name}</b></font></span>
+        </Popup>
+      </Marker>
+    })
+
+
+    let userElement;
     if (this.props.user){
-      userElement = <CircleMarker center={this.props.user.pos}/>
+      userElement = <CircleMarker radius={15} center={this.props.user.pos}/>;
     } else {
-      userElement = ''
+      userElement = '';
     }
 
     // Note: .bind(this) is important for the handler function's 'this'
@@ -31,6 +61,7 @@ class MapView extends React.Component {
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+        {restaurantsElements}
         {providerElements}
         {userElement}
       </Map>
